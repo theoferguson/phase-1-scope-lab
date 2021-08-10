e769ca64e86d5f40700cc67b0784eb4540ea6dc7
Theo Ferguson <ferguson.theo@gmail.com>
2021-08-10 17:46:11 -0400

    commit tree f483986d0560c8c63cb04430e76c46a7ec4de12b

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = "bob"
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+    return customerName;
+}
+
+var bestCustomer;
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = "eric"
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "boogie"
+}
